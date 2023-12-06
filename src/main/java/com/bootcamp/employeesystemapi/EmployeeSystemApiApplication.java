78439e189f78536771b4d2b4e3b5d98809750f79
tmampa <tmampa94@gmail.com>
2023-12-06 19:40:58 +0200

    Simple calculator

--- src/main/java/com/bootcamp/employeesystemapi/EmployeeSystemApiApplication.java
+++ src/main/java/com/bootcamp/employeesystemapi/EmployeeSystemApiApplication.java
@@ -3,11 +3,55 @@ package com.bootcamp.employeesystemapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class EmployeeSystemApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeeSystemApiApplication.class, args);
+		// SpringApplication.run(EmployeeSystemApiApplication.class, args);
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter first number: ");
+		double num1 = scanner.nextDouble();
+
+		System.out.println("Enter second number: ");
+		double num2 = scanner.nextDouble();
+
+		System.out.println("Choose (+, -, *, /}: ");
+		char operator = scanner.next().charAt(0);
+
+		scanner.close();
+		double result = 0;
+
+		switch (operator)
+		{
+			case '+':
+				result = num1 + num2;
+				break;
+
+			case '-':
+				result = num1 - num2;
+				break;
+
+			case '*':
+				result = num1 * num2;
+				break;
+
+			case '/':
+				if (num2 != 0)
+					result = num1 / num2;
+				else {
+					System.out.println("Error! Dividing by zero is not allowed.");
+					return;
+				}
+				break;
+
+			default:
+				System.out.println("Error! Invalid operator. Please enter correct operator.");
+		}
+		System.out.println(result);
 	}
 
 }
